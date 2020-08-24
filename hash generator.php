7ffed81da4008987a4b1dd6b9e84320a392fbb5e
Tuntufye Mwanyongo <58251346+Tuntufye4@users.noreply.github.com>
2020-08-24 14:45:01 +0200

    Add files via upload

---
+++ hash generator.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['user_password']) && !empty($_POST['user_password'])){
+	$user_password =md5 ($_POST['user_password']);
+
+	$filename = 'hash.txt';
+	$handle = fopen($filename, 'r');
+	$file_password =fread($handle, filesize($filename));
+}else{
+	echo 'Please enter a password.';
+}
+
+
+
+?>
+
+<form action="index.php"  method="POST">
+     Password: <input type="text" name="user_password"><br><br>
+     <input type="submit" value="Submit">
+</form>
